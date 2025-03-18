1ef0fd39f26370ec40fdfee93f2a5f3ed24be194
kennethlerch <157252265+kennethlerch@users.noreply.github.com>
2025-03-18 16:05:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def check_status():
     return jsonify({"script_running": script_running})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
